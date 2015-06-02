9f9a18944634cab5075e3aeca49d4a5ce277d862
Nuria Arranz-Velazquez <nuria@opusvl.com>
2015-06-02 13:06:26 +0100

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+{
+   'name': 'UK customised reports',
+   'version': '0.1',
+   'category': 'Reporting',
+   'description': """
+   Customised reports for OpusVL:
+    - Sales order / Quotation
+    - Account balance / overdue
+    - Invoice print 
+
+    These have been migrated from V3.3, V4, V5, V6, V6.1 to V8
+
+   """,
+   'author': 'OpusVL',
+   'website': 'http://opusvl.com',
+   'depends': ['account_accountant', 'sale', 'report'],
+   'init_xml': [],
+   'update_xml': [
+       'reports.xml',
+   ],
+   'demo_xml': [],
+   'test': [],
+   'installable': True,
+   'active': False,
+}
---
+++ report/__init__.py
@@ -0,0 +1,1 @@
+import UK_Reports
